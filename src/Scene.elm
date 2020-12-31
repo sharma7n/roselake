47d843d1d8c7739f08a9dde3485d2324714b4442
Nik Sharma <sharma7n@gmail.com>
2020-12-31 12:36:09 -0600

    remove unused imports from Scene

--- src/Scene.elm
+++ src/Scene.elm
@@ -2,16 +2,6 @@ module Scene exposing
     ( Scene(..)
     )
 
-import Action exposing (Action)
-import Battle exposing (Battle)
-import DelvePhase exposing (DelvePhase)
-import Delve exposing (Delve)
-import Monster exposing (Monster)
-import Reward exposing (Reward)
-import Shop exposing (Shop)
-import BossPhase exposing (BossPhase)
-import BossState exposing (BossState)
-
 type Scene
     = Player
     | Essentia
